0b3766d4cb58660f8e4763d8e69b342b3258188a
bravevalley <dassyareg@gmail.com>
2025-02-07 19:35:12 +0100

    added cors

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func main() {
 	// Configure and start webserver.
 	r := gin.Default()
 
+	// CORS
+	r.Use(CorsMiddleware())
+
 	r.GET("/api/classify-number", DigiProp)
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
